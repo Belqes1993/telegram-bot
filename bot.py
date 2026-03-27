0f244965b8890a12b9f81f9f53417e8a4c41b8ff
Belqes1993 <belqesahmed1993@gmail.com>
2026-03-27 12:27:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot 
 token ="8707408461:AAF__6f4vtbxGHMz5Rdchz-Ps9DrKA7oTGk"
-bot = telebot.teleBot(token)
+bot = Telebot.teleBot(token)
 @bot.message_handler(commands=['start'])
 def start(massage):
   bot.reply_to(message,"hello in learning _both of my own ..")

02c03ac93edb27c5a35bf781772cdb8af067c2f6
Belqes1993 <belqesahmed1993@gmail.com>
2026-03-27 13:16:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot 
 token ="8707408461:AAF__6f4vtbxGHMz5Rdchz-Ps9DrKA7oTGk"
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(massage):
   bot.reply_to(message,"hello in learning _both of my own ..")
 bot.infinity_polling()
